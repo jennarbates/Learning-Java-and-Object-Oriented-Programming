e534948c087a40029dd25917bc2f89fbafc3ab1f
jennarbates <jennaraquelbates@gmail.com>
2021-09-17 00:23:33 -0400

    added rounding to calcDistance

--- Notes/Method-Notes/src/main/java/methodnotes/DistanceFormula.java
+++ Notes/Method-Notes/src/main/java/methodnotes/DistanceFormula.java
@@ -1,5 +1,13 @@
 package methodnotes;
 
+/*
+Write a program that asks the user to enter two sets of x,y coords
+and prints out the distance between those coords.
+Write a program that asks the user to enter two 2d points
+and prints out the distance between those 2d points.
+ */
+
+
 import java.util.Scanner;
 
 public class DistanceFormula
@@ -12,41 +20,42 @@ public class DistanceFormula
     public static double calcDelta(int x, int y)
     {
         return Math.abs(x - y);
-        //look into what data type Math.abs returns
+        //look into what data type Math.abs returns -> apparently it returns the same type of whatever you put in the parameter but how does it have a variable return type?
     }
 
     public static double calcDistance(int x1, int y1, int x2, int y2)
     {
-        return Math.sqrt(calcDelta(x1,y1) + Math.sqrt(calcDelta(x2,y2)));
+        return Math.round(Math.sqrt(calcDelta(x1,y1) + Math.sqrt(calcDelta(x2,y2))));
     }
 
-    public static String makeInputRequest(String request)
+    public static String makeInputRequestString(String request)
     {
         return "Please input " + request;
         //this makes unit testing easier since you can't do unit testing on anything that uses scanner since scanner is private
     }
 
-    private static int getUserInput(String request)
+    private static int getUserInt(String request)
     {
 
-
-        System.out.println(makeInputRequest(request));
-        //switch statement to determine type from type parameter and request input of that type
+        System.out.println(makeInputRequestString(request));
+        return kb.nextInt();
     }
 
     private static int[] coordReader()
     {
         int[] coord = new int[2];
-        coord[0] = getUserInput("the x value");
-        coord[1] = getUserInput("the y value");
+        coord[0] = getUserInt("the x value");
+        coord[1] = getUserInt("the y value");
         return coord;
-
     }
 
-
-    public static int[] main(String[] args)
+    public static void main(String[] args)
     {
-
+        System.out.println("First Coordinate:");
+        int[] coord1 = coordReader();
+        System.out.println("First Coordinate:");
+        int[] coord2 = coordReader();
+        System.out.println("The distance between (" + coord1[0] + "," + coord1[1] + ") and (" + coord2[0] + "," + coord2[1] + ") is " + calcDistance(coord1[0],coord1[1],coord2[0],coord1[1]) + " units.");
 
     }
 }
