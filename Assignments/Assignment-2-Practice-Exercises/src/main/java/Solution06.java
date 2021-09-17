32083f85d038ce98ea5887cf7c8b651343c33b92
jennarbates <jennaraquelbates@gmail.com>
2021-09-17 02:29:50 -0400

    array formatting

--- Assignments/Assignment-2-Practice-Exercises/src/main/java/Solution06.java
+++ Assignments/Assignment-2-Practice-Exercises/src/main/java/Solution06.java
@@ -22,7 +22,7 @@ Handle situations where the program returns a negative number by stating that th
  */
 public class Solution06
 {
-    public static void main(String args[])
+    public static void main(String[] args)
     {
 
     }
