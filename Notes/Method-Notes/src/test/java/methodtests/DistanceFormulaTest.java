b8b06c5c280b7804a13b1e110f47e1cdbd7a6e07
jennarbates <jennaraquelbates@gmail.com>
2021-09-17 02:13:26 -0400

    rename Assignments

--- Notes/Method-Notes/src/test/java/methodtests/DistanceFormulaTest.java
+++ Notes/Method-Notes/src/test/java/methodtests/DistanceFormulaTest.java
@@ -1,6 +1,6 @@
 package methodtests;
 
-import
+import methodnotes.DistanceFormula;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,9 +13,10 @@ public class DistanceFormulaTest
 {
     // You want to have a test for every public method in your program
 
-    @test
+   /* @test
     void calcDistance()
     {
+        DistanceFormula form = new DistanceFormula();
         double expected = 10.20;
         double actual = calcDistance(7.0,4.0,17.0,6.0);
 
@@ -23,6 +24,6 @@ public class DistanceFormulaTest
     }
 
 
-
+*/
 
 }
