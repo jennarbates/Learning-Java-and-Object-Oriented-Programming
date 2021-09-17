7d140f8305a97d5f2cf08c5fd07d53f918b49c86
jennarbates <jennaraquelbates@gmail.com>
2021-09-17 04:46:42 -0400

    boilerplate

--- Assignments/Assignment-2-Practice-Exercises/src/main/java/main/Application.java
+++ Assignments/Assignment-2-Practice-Exercises/src/main/java/main/Application.java
@@ -1,12 +1,9 @@
-package main;/*
+/*
  *  UCF COP3330 Fall 2021 Assignment 2 Solution
  *  Copyright 2021 Jenna Bates
  */
 
-
-
-
-
+package main;
 
 import java.time.LocalDate;
 import java.util.Scanner;
