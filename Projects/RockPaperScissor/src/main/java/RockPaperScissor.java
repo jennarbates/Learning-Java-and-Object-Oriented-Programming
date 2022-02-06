f3fad7e71e2246974d0f9b3a5ff7d1cc8d5ee077
unknown <jennaraquelbates@gmail.com>
2022-02-06 02:56:06 -0500

    playRound method added with basic logic

--- Projects/RockPaperScissor/src/main/java/RockPaperScissor.java
+++ Projects/RockPaperScissor/src/main/java/RockPaperScissor.java
@@ -7,11 +7,13 @@ public class RockPaperScissor {
     private int game;
     Random rand = new Random();
     Scanner input = new Scanner(System.in);
-
+    private boolean playing = true;
+    private int roundNum = 0;
     public static final String GREETING = "Welcome to the Rock...Paper...Scissor Game! Let me tell you the rules to this game. You and I will get to \n" +
             "choose rock, paper, or scissor. After we made a decision, we will then reveal the choices we both made and \n" +
             "decide a winner based on these rules. Rock beats Scissor! Paper beats Rock! Scissor beats Paper! If we both \n" +
             "pick the same option, then it is a tie. Ready to play? Here we go! I've been told by users that I'm really good!";
+
 
     public RockPaperScissor(){
         super();
@@ -33,9 +35,28 @@ public class RockPaperScissor {
         this.game = game;
     }
 
-
+    //methods
 
     public static void greeting(){
         System.out.println(GREETING);
     }
+
+    public void playRound(){
+        int computerWins = 0;
+        int userWins = 0;
+        System.out.println("Welcome to game " + game + "!");
+        while(round<3){
+            //switch( choiceBattle(userChoice(),computerChoice()) ){
+            //case 0: tie;
+                //break;
+            //case 1: user;
+                //break;
+           //case 2: pc;
+                //break;
+            //ask about if throwing an exception in the default case makes sense
+           // }
+            round++;
+        }
+    }
+
 }
