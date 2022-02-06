8a0cfca2ffc661266c321f176348880d18653f18
unknown <jennaraquelbates@gmail.com>
2022-02-06 02:35:50 -0500

    created greeting() method and GREETING final String var to hold greeting

--- Projects/RockPaperScissor/src/main/java/RockPaperScissor.java
+++ Projects/RockPaperScissor/src/main/java/RockPaperScissor.java
@@ -8,6 +8,11 @@ public class RockPaperScissor {
     Random rand = new Random();
     Scanner input = new Scanner(System.in);
 
+    public static final String GREETING = "Welcome to the Rock...Paper...Scissor Game! Let me tell you the rules to this game. You and I will get to \n" +
+            "choose rock, paper, or scissor. After we made a decision, we will then reveal the choices we both made and \n" +
+            "decide a winner based on these rules. Rock beats Scissor! Paper beats Rock! Scissor beats Paper! If we both \n" +
+            "pick the same option, then it is a tie. Ready to play? Here we go! I've been told by users that I'm really good!";
+
     public RockPaperScissor(){
         super();
         //ask about using this() in default constructor & as a method
@@ -26,5 +31,11 @@ public class RockPaperScissor {
         this.userWins = userWins;
         this.computerWins = computerWins;
         this.game = game;
+    }
+
+
+
+    public static void greeting(){
+        System.out.println(GREETING);
     }
 }
