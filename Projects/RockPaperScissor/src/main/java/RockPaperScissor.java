1cb0684a7a57466fcc043c92c1c2cc4db898358c
unknown <jennaraquelbates@gmail.com>
2022-02-06 02:31:35 -0500

    RockPaperScissor class constructors added

--- Projects/RockPaperScissor/src/main/java/RockPaperScissor.java
+++ Projects/RockPaperScissor/src/main/java/RockPaperScissor.java
@@ -10,8 +10,21 @@ public class RockPaperScissor {
 
     public RockPaperScissor(){
         super();
-
+        //ask about using this() in default constructor & as a method
+        userWins = 0;
+        computerWins = 0;
+        game = 1;
     }
-
-
+    public RockPaperScissor(int userWins, int computerWins){
+        super();
+        this.userWins = userWins;
+        this.computerWins = computerWins;
+        this.game = 0;
+    }
+    public RockPaperScissor(int userWins, int computerWins, int game){
+        super();
+        this.userWins = userWins;
+        this.computerWins = computerWins;
+        this.game = game;
+    }
 }
