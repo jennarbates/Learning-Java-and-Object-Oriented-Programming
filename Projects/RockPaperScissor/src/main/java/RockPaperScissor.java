0ac947d1579d441df038615e3a6d8de19855be2f
unknown <jennaraquelbates@gmail.com>
2022-02-06 02:15:42 -0500

    Created RockPaperScissors Project and started class contstructors

---
+++ Projects/RockPaperScissor/src/main/java/RockPaperScissor.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RockPaperScissor {
+    private int userWins;
+    private int computerWins;
+    private int game;
+    Random rand = new Random();
+    Scanner input = new Scanner(System.in);
+
+    public RockPaperScissor(){
+        super();
+
+    }
+
+
+}
