434ea46e72b039432d59c9136703ad112b9dff29
unknown <jennaraquelbates@gmail.com>
2022-02-10 21:33:49 -0500

    added Header for assignment

---
+++ Projects/VigenereCipher/VigenereCipher.java
@@ -0,0 +1,21 @@
+/*=============================================================================
+| Assignment: pa01 - Encrypting a plaintext file using the Vigenere cipher
+|
+| Author: Jenna Bates
+| Language: Java
+|
+| To Compile: javac pa01.java
+| gcc -o pa01 pa01.c
+| g++ -o pa01 pa01.cpp
+|
+| To Execute: java -> java pa01 kX.txt pX.txt
+| where kX.txt is the keytext file
+| and pX.txt is plaintext file
+|
+| Note: All input files are simple 8 bit ASCII input
+|
+| Class: CIS3360 - Security in Computing - Spring 2022
+| Instructor: McAlpin
+| Due Date: Feb 27, 2022
+|
++=============================================================================*/
