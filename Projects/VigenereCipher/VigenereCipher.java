2b6951713bc64267fd8b7a665aac633dda76fe34
unknown <jennaraquelbates@gmail.com>
2022-02-10 21:35:00 -0500

    added footer with Academic Integrity Statement minus NID

--- Projects/VigenereCipher/VigenereCipher.java
+++ Projects/VigenereCipher/VigenereCipher.java
@@ -19,3 +19,16 @@
 | Due Date: Feb 27, 2022
 |
 +=============================================================================*/
+
+
+
+
+
+/*=============================================================================
+| I Jenna Bates (NID) affirm that this program is
+| entirely my own work and that I have neither developed my code together with
+| any another person, nor copied any code from any other person, nor permitted
+| my code to be copied or otherwise used by any other person, nor have I
+| copied, modified, or otherwise used programs created by others. I acknowledge
+| that any violation of the above terms will be treated as academic dishonesty.
++=============================================================================*/
