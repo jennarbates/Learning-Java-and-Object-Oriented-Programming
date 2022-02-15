2ca03e0526b53ec88f76520e6cbde6d7c1670fb3
unknown <jennaraquelbates@gmail.com>
2022-02-14 22:00:27 -0500

    added all required method headers and explanations

--- Projects/TextAnalysis/TextAnalysis.java
+++ Projects/TextAnalysis/TextAnalysis.java
@@ -27,10 +27,47 @@ public class TextAnalysis{
     // "This attribute stores the max number of sentences to observe for the class object. Each class object will have a different limit."
     private int limit;
 
-
     public TextAnalysis(int limit, String[] data){
         this.limit = limit;
         this.data = data;
     }
+
+    //This non-static method will display the content of text document stored in the class object data attribute. Each sentence will be displayed on its own line.
+    public void display(){
+
+    }
+
+    //This non-static method will display the content of the analyses done in the text stored for the respective class object in a simple table format.
+    public void tableDisplay(){
+
+    }
+
+    //This non-static overloaded method will analyze the words in the text document. This method will count the number of words used in each sentence.
+    // The result of each sentence will be stored in the wordsize attribute. Do not count the special characters as part of the words. Your program
+    //should also not count digits (101, 555, etc...). This information will get displayed in tableDisplay()
+    public void letterAnalysis(){
+
+    }
+
+    //This non-static overloaded method will analyze the words in the text document. This method will count the number of words used in each sentence.
+    //The result of each sentence will be stored in the wordsize attribute. Do not count the special characters as part of the words. Your program
+    //should also not count digits (101, 555, etc...). This information will get displayed in tableDisplay()
+    public void wordAnalysis(){
+
+    }
+
+    //This non-static overloaded method will analyze the words in the text document. This method will measure the length of each sentence.
+    //In simple terms you will count the number of words contained in a sentence. You can assume that all sentences will have at least one word. This
+    //method has one parameter of type int called num. The parameter num holds the number of sentences to be observed. For example, if data has 20 sentences
+    //and num is passed 7, then the first 7 sentences will be analyzed. The result of each sentence will be stored in the wordcount
+    //attribute. This information will get displayed in tableDisplay(). Make sure num is a positive number and that it does not exceed the limit attribute.
+    //If the condition is not satisfied, the message “Exceeded and cannot produce an analysis on this component” Should be displayed to the terminal.
+    public void wordAnalysis(int num){
+
+    }
+
+
+
+
 
 }
